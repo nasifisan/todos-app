94523fdf6cc44a2c6aa83b9612448d064badc620
Nasif Mahmud Nipom <nasif.nipom@gmail.com>
2022-11-18 15:40:45 +0600

    Initial setup 2

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+<<<<<<< Updated upstream
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+=======
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+>>>>>>> Stashed changes
